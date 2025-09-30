4cfba27eb489dc9624eaa91d1fa5d2d9dec76a25
celine1906 <reginaceline1906@gmail.com>
2025-09-30 13:19:13 +0700

    Initial Commit
    Initial Project Settings

---
+++ PublicSpeakingAPP/PublicSpeakingAPPTests/PublicSpeakingAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  PublicSpeakingAPPTests.swift
+//  PublicSpeakingAPPTests
+//
+//  Created by Regina Celine Adiwinata on 30/09/25.
+//
+
+import Testing
+@testable import PublicSpeakingAPP
+
+struct PublicSpeakingAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
